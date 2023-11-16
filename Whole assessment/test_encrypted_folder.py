0d33548d433389500d9e0ed175933d5ebba42ac0
davide-marchi <davidemarchi00@gmail.com>
2023-11-17 00:37:42 +0100

    Possibile decrypt dei file fatti con gpg

--- Whole assessment/test_encrypted_folder.py
+++ Whole assessment/test_encrypted_folder.py
@@ -5,3 +5,25 @@ import pandas as pd
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 df = pd.read_csv('file.csv')
 print(df)
+
+"""
+import gnupg
+import csv
+import io
+
+# Initialize GnuPG
+gpg = gnupg.GPG()
+
+# Decrypt the file
+with open('yourfile.csv.gpg', 'rb') as f:
+    decrypted_data = gpg.decrypt_file(f, passphrase='your_passphrase')
+
+# Check if decryption was successful
+if not decrypted_data.ok:
+    raise ValueError("Decryption failed")
+
+# Read the decrypted data as CSV
+csv_reader = csv.reader(io.StringIO(str(decrypted_data)))
+for row in csv_reader:
+    print(row)
+"""
