0134eafe20183fbcc52d472d5b6a682b66047cf2
Davide <davidemarchi00@gmail.com>
2024-04-30 00:16:35 +0000

    Small adjuctment to run the demo on the remote SSH

--- Whole assessment/main_train_plot.py
+++ Whole assessment/main_train_plot.py
@@ -8,8 +8,8 @@ from plotting import plot_dashboards, plot_corrcoeff
 # Cambio la directory di esecuzione in quella dove si trova questo file
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-data_folder = 'C:/Users/david/Documents/University/Borsa di Studio - REDCap/only_AC-80_patients/'
-#data_folder = '../../only_AC-80_patients/'
+#data_folder = 'C:/Users/david/Documents/University/Borsa di Studio - REDCap/only_AC-80_patients/'
+data_folder = '../../AInCP-Training/only_AC-80_patients/'
 
 min_mean_test_score = 0.0 #TODO: change to 0.85
 window_size = 300
