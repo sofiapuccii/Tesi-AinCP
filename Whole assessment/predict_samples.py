c05c69bf2ce1c75ab463848acdfe1cb78042d9a0
Davide <davidemarchi00@gmail.com>
2024-05-03 15:57:55 +0000

    Optimized predict_samples.py ! Now it uses np.arrays to perfomr operations and makes some checks in a more efficient way. Could still be improved a lot. The function in not very well coded For example it makes the same windows multiple times instead of making deep copies etc..

--- Whole assessment/predict_samples.py
+++ Whole assessment/predict_samples.py
@@ -4,12 +4,6 @@ from elaborate_magnitude import elaborate_magnitude
 
 def predict_samples(data_folder, estimators, patient):
 
-    df = pd.read_csv(data_folder + 'data/' + str(patient) + '_week_1sec.csv')
-    magnitude_D = np.sqrt(np.square(df['x_D']) + np.square(df['y_D']) + np.square(df['z_D']))
-    magnitude_ND = np.sqrt(np.square(df['x_ND']) + np.square(df['y_ND']) + np.square(df['z_ND']))
-
-    to_discard = []
-
     if not estimators:
         print('You have selected zero estimators to predict the samples with')
         exit(1)
@@ -17,6 +11,12 @@ def predict_samples(data_folder, estimators, patient):
     if len(set(es['window_size'] for es in estimators)) != 1:
         print('You have selected estimators that operates on different window sizes')
         exit(1)
+
+    df = pd.read_csv(data_folder + 'data/' + str(patient) + '_week_1sec.csv')
+    magnitude_D = np.sqrt(np.square(np.array(df['x_D'])) + np.square(np.array(df['y_D'])) + np.square(np.array(df['z_D'])))
+    magnitude_ND = np.sqrt(np.square(np.array(df['x_ND'])) + np.square(np.array(df['y_ND'])) + np.square(np.array(df['z_ND'])))
+
+    to_discard = []
 
     window_size = estimators[0]['window_size']
 
@@ -26,15 +26,15 @@ def predict_samples(data_folder, estimators, patient):
     # Fase di chunking
     for j in range (0, len(magnitude_D), window_size):
 
-        chunk_D = magnitude_D.iloc[j:j + window_size]
-        chunk_ND = magnitude_ND.iloc[j:j + window_size]
+        chunk_D = magnitude_D[j:j + window_size]
+        chunk_ND = magnitude_ND[j:j + window_size]
 
         if chunk_D.size == window_size and chunk_ND.size == window_size:
             
             for es in estimators:
                 es['series'].append(elaborate_magnitude(es['method'], chunk_D, chunk_ND))
 
-            if chunk_D.agg('sum') == 0 and chunk_ND.agg('sum') == 0:
+            if np.all(chunk_D == 0) and np.all(chunk_ND == 0):
                 to_discard.append(int(j/window_size))
 
     y_list = []
