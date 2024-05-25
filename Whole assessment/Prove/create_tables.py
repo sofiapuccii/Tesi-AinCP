d5feef4613e38c398d7fb3707d934d561f855215
Davide <davidemarchi00@gmail.com>
2024-05-25 17:31:56 +0000

    Creating the tables as csvs

--- Whole assessment/Prove/create_tables.py
+++ Whole assessment/Prove/create_tables.py
@@ -13,15 +13,17 @@ for iteration in range(10):
     filtered_results = best_estimators_results[(best_estimators_results['mean_test_score'] >= 0.85) & (best_estimators_results['window_size'] == 300)].copy()
     filtered_results['mean_test_score'] = filtered_results['mean_test_score'].round(4)
     filtered_results['std_test_score'] = filtered_results['std_test_score'].round(4)
-    results.append(filtered_results)
-    results.append(pd.Series('-------------------------------------------------------------------------------------'))
-    results.append(pd.Series('-------------------------------------------------------------------------------------'))
-    results.append(pd.Series('-------------------------------------------------------------------------------------'))
+
+    filtered_results.rename(columns={'model_type': 'Model type', 'window_size': 'Window size', 'method': 'Method', 'params': 'Parameters', 'mean_test_score': 'MTS', 'std_test_score': 'STD'}, inplace=True)
+
+    filtered_results.to_csv(save_folder + 'iter'+str(iteration)+'_dataframe.csv', index=False)
+
+
 
 # Concatenate all dataframes
-all_results = pd.concat(results)
+#all_results = pd.concat(results)
 
 # Save to txt
-all_results.to_csv('all_results.txt', index=False, sep='\t')
+#all_results.to_csv('all_results.txt', index=False, sep='\t')
 
 
